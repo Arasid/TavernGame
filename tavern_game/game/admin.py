8e78c5e21ce339ee8fedc372c736d14ac55ec9d2
Arasid <basa.akrobat@gmail.com>
2018-03-02 10:52:20 +0100

    Add models.

--- tavern_game/game/admin.py
+++ tavern_game/game/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Person, Ration, BarPurchase, RichPerson
+
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+class RationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'timestamp', 'person', 'value')
+
+class BarPurchaseAdmin(admin.ModelAdmin):
+    list_display = ('id', 'timestamp', 'person', 'value')
+
+class RichPersonAdmin(admin.ModelAdmin):
+    list_display = ('id', 'timestamp', 'person', 'value')
+
+admin.site.register(Person, PersonAdmin)
+admin.site.register(Ration, RationAdmin)
+admin.site.register(BarPurchase, BarPurchaseAdmin)
+admin.site.register(RichPerson, RichPersonAdmin)
--- tavern_game/game/models.py
+++ tavern_game/game/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+    def __unicode__(self):
+        return self.name
+
+
+class Ration(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    value = models.IntegerField()
+
+    def __str__(self):
+        return '(Ration-%s-%s-%s)' % (self.timestamp, self.person, self.value)
+
+    def __unicode__(self):
+        return '(Ration-%s-%s-%s)' % (self.timestamp, self.person, self.value)
+
+
+class BarPurchase(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    value = models.IntegerField()
+
+    def __str__(self):
+        return '(BarPurchase-%s-%s-%s)' % (self.timestamp, self.person, self.value)
+
+    def __unicode__(self):
+        return '(BarPurchase-%s-%s-%s)' % (self.timestamp, self.person, self.value)
+
+
+class RichPerson(models.Model):
+    person = models.OneToOneField(Person, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now=True)
+    value = models.IntegerField()
+
+    def __str__(self):
+        return '(RichPerson-%s-%s-%s)' % (self.timestamp, self.person, self.value)
+
+    def __unicode__(self):
+        return '(RichPerson-%s-%s-%s)' % (self.timestamp, self.person, self.value)
